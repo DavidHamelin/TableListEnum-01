b373b6e740d0c37d138dbd7605c2b1057d75b179
DavidHamelin <hamelindavid@hotmail.fr>
2018-12-18 13:28:29 +0100

    correction

--- Program.cs
+++ Program.cs
@@ -15,6 +15,17 @@ namespace TabEx01
             {
                 Console.WriteLine(week[i]);
             }
+
+            /*
+            string[] week1 = new string[7];
+            week1[0] = "Lundi";
+            week1[1] = "Mardi";
+            week1[2] = "Mercredi";
+            week1[3] = "Jeudi";
+            week1[4] = "Vendredi";
+            week1[5] = "Samedi";
+            week1[6] = "Dimanche";
+            */
         }
     }
 }
